8f57d5a4aaa23cc0799031a74666cb4818e7eede
Breno Meneses <brenopinheirodemeneses@gmail.com>
2023-05-19 15:18:38 -0300

    Update

--- simulate.m
+++ simulate.m
@@ -4,7 +4,7 @@ warning('off','fuzzy:dialogs:warnEvalfis_NoRuleFired')
 %% Parâmetros
 R = 0.195/2; L = 0.331;
 
-emin = 0.5;
+emin = 0.05;
 u_max = 1.2;
 omega_max = (pi/180)*300;
 
@@ -12,7 +12,7 @@ gamma = 0.3; k = 1; % Parâmetros do controlador não-linear (Lyapunov)
 
 %% Controlador Fuzzy
 Tracking = readfis('Tracking');
-Obstacle = readfis('Obstacle2');
+Obstacle = readfis('Obstacle');
 DeadLock = readfis('DeadLock');
 Fuzzy_Behavior = readfis('Fuzzy_Behavior');
 %% Simulação
